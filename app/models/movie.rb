2b38f8243e71acef62ef6f10c86516472a6d1015
JStern95 <JSternberg95@gmail.com>
2018-09-13 11:36:24 -0400

    Done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,11 +5,31 @@ class Movie
 
   def initialize(title)
     @title = title
-    self.class.all << self
+    @@all.push(self)
   end
 
   def self.all
     @@all
+  end
+
+  def queue_items
+    QueueItem.all.select{|qi| qi.movie ==self}
+  end
+
+  def viewers
+    queue_items.map {|qi| qi.viewer}
+  end
+
+  def average_rating
+    total=queue_items.length
+    total_score= 0.to_f
+    queue_items.each {|qi| total_score += qi.rating}
+    avg= total_score/total
+  end
+
+  def self.highest_rated
+    top_rated=self.all.map {|m| m.average_rating}.sort.reverse[0]
+    self.all.find{|m| m.average_rating == top_rated}
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,6 +4,32 @@ def reload
   load 'config/environment.rb'
 end
 
+v1= Viewer.new("YaBoi6262")
+v2= Viewer.new("PrincessViewer")
+v3= Viewer.new("lolwhodidthis")
+v4= Viewer.new("GenericUsername")
+v5= Viewer.new("hahahehehahaho")
+v6= Viewer.new("HorrorLover39")
+
+m1= Movie.new("Love Actually")
+m2= Movie.new("Invasion of the Body Snatchers")
+m3= Movie.new("Harry Potter pt 1")
+m4= Movie.new("Generic Movie")
+m5= Movie.new("The Avengers")
+m6= Movie.new("Saw")
+
+qi1= QueueItem.new(v1,m1,3)
+qi2= QueueItem.new(v2,m1,5)
+qi3= QueueItem.new(v3,m5,5)
+qi4= QueueItem.new(v5,m5,1)
+qi5= QueueItem.new(v2,m5,4)
+qi6= QueueItem.new(v4,m2,2)
+qi7= QueueItem.new(v5,m3,1)
+qi8= QueueItem.new(v2,m4,1)
+qi9= QueueItem.new(v4,m6,2)
+
+
+
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
